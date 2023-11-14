3a370784a5be3779ff86845f72f4fb90dbfa12c2
Thedogecraft <dantaealt@gmail.com>
2023-11-13 16:03:03 -0800

    Initial commit

---
+++ core.js
@@ -0,0 +1,10 @@
+
+// Set the Tab title if the Tab cloak data is there
+if (tabData.title) {
+  document.title = tabData.title;
+}
+
+// Set the Tab icon if the Tab cloak data is there
+if (tabData.icon) {
+  document.querySelector('link[rel="icon"]').href = tabData.icon;
+}
